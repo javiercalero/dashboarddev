171a81bcba8cdf5adca935eadc55da7474db2701
Javier Calero <javiercalero@hotmail.com>
2016-12-21 15:46:41 -0300

    556489412

---
+++ Task_Dashboard/app/Tasks_Component.js
@@ -0,0 +1,65 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+const core_1 = require("@angular/core");
+const app_dataservice_1 = require("./app.dataservice");
+const task_1 = require("./models/task");
+const app_constants_1 = require("./app.constants");
+let AppComponent = class AppComponent {
+    constructor(_dataService) {
+        this._dataService = _dataService;
+    }
+    ngOnInit() {
+        this.getAllItems();
+    }
+    getAllItems() {
+        this._dataService
+            .GetAll()
+            .subscribe((data) => {
+            this.myItems = data;
+            for (let task of this.myItems) {
+                task.disableEdit = true;
+            }
+        }, error => console.log(error), () => console.log('Get all Items complete'));
+    }
+    getItemById(id) {
+        var myTask = new task_1.Task();
+        this._dataService.GetSingle(id).subscribe((data) => myTask = data, error => console.log(error), () => console.log('Get Item by id complete'));
+        return myTask;
+    }
+    addNewTask(name) {
+        if (name.value != undefined && name.value != "" && name.value != 'Add a new task') {
+            var myTask = new task_1.Task();
+            myTask.name = name.value;
+            this._dataService.Add(myTask).subscribe((data) => { myTask = data; this.getAllItems(); }, error => console.log(error), () => console.log());
+        }
+    }
+    updateTask(myTask) {
+        this._dataService.Update(myTask).subscribe((data) => { myTask = data; this.getAllItems(); }, error => console.log(error), () => console.log());
+    }
+    deleteTask(id) {
+        this._dataService.Delete(id);
+    }
+    setEditState(myTask, state) {
+        var lookupTask = [myTask].find(task => task._id == myTask._id);
+        if (lookupTask != undefined)
+            lookupTask.disableEdit = state;
+    }
+};
+AppComponent = __decorate([
+    core_1.Component({
+        providers: [app_dataservice_1.DataService, app_constants_1.Configuration],
+        selector: 'my-app',
+        templateUrl: './views/tasklist.html',
+    }),
+    __metadata("design:paramtypes", [app_dataservice_1.DataService])
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=Tasks_Component.js.map
---
+++ Task_Dashboard/routes/apiController.js
@@ -0,0 +1,149 @@
+"use strict";
+var url = process.env.MONGO_DB_CONNECT;
+var ObjectId = require('mongodb').ObjectID;
+function getTaskById(req, res) {
+    var tareas;
+    var MongoClient = require('mongodb').MongoClient, co = require('co'), assert = require('assert');
+    co(function* () {
+        var db = yield MongoClient.connect(url);
+        console.log("Connected correctly to server");
+        db.collection('Tasks').findOne({ '_id': new ObjectId(req.params.id) }, function (err, cursor) {
+            res.json(cursor);
+        });
+        db.close();
+    }).catch(function (err) {
+        console.log(err.stack);
+    });
+}
+exports.getTaskById = getTaskById;
+;
+function getTasks(req, res) {
+    var MongoClient = require('mongodb').MongoClient, co = require('co'), assert = require('assert');
+    co(function* () {
+        var db = yield MongoClient.connect(url);
+        console.log("Connected correctly to server");
+        db.collection('Tasks').find({}, function (err, cursor) {
+            cursor.toArray(function (err, results) {
+                res.json(results);
+            });
+        });
+        db.close();
+    }).catch(function (err) {
+        console.log(err.stack);
+    });
+}
+exports.getTasks = getTasks;
+;
+function updateTaskById(req, res) {
+    var task = req.body;
+    var updObj = { _id: ObjectId, name: "", despcription: "", status: "", user: "", created_date: "", priority: "" };
+    if (task._id != undefined) {
+        updObj._id = new ObjectId(task._id);
+    }
+    if (task.status != undefined) {
+        updObj.status = task.status;
+    }
+    if (task.name != undefined) {
+        updObj.name = task.name;
+    }
+    if (task.description != undefined) {
+        updObj.despcription = task.description;
+    }
+    if (task.user != undefined) {
+        updObj.user = task.user;
+    }
+    if (task.priority != undefined) {
+        updObj.priority = task.priority;
+    }
+    if (!updObj) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    }
+    else {
+        var MongoClient = require('mongodb').MongoClient, co = require('co'), assert = require('assert');
+        co(function* () {
+            var db = yield MongoClient.connect(url);
+            console.log("Connected correctly to server");
+            db.collection('Tasks').update({
+                '_id': new ObjectId(task._id)
+            }, updObj, {}, function (err, result) {
+                if (err) {
+                    res.send(err);
+                }
+                else {
+                    res.json(result);
+                }
+            });
+        }).catch(function (err) {
+            console.log(err.stack);
+        });
+    }
+}
+exports.updateTaskById = updateTaskById;
+;
+function addTask(req, res) {
+    var task = req.body;
+    var updObj = { name: "", description: "", status: "", user: "", priority: "" };
+    if (task.status != undefined) {
+        updObj.status = task.isCompleted;
+    }
+    if (task.name) {
+        updObj.name = task.name;
+    }
+    if (!updObj) {
+        res.status(400);
+        res.json({
+            "error": "Invalid Data"
+        });
+    }
+    else {
+        var MongoClient = require('mongodb').MongoClient, co = require('co'), assert = require('assert');
+        co(function* () {
+            var db = yield MongoClient.connect(url);
+            console.log("Connected correctly to server");
+            db.collection('Tasks').insertOne({
+                'name': updObj.name,
+                'description': '',
+                'status': '',
+                'user': '',
+                'created_date': new Date().toISOString().slice(0, 10),
+                'priority': ''
+            }, updObj, {}, function (err, result) {
+                if (err) {
+                    res.send(err);
+                }
+                else {
+                    res.json(result);
+                }
+            });
+        }).catch(function (err) {
+            console.log(err.stack);
+        });
+    }
+}
+exports.addTask = addTask;
+;
+function deleteTask(req, res) {
+    var MongoClient = require('mongodb').MongoClient, co = require('co'), assert = require('assert');
+    co(function* () {
+        var db = yield MongoClient.connect(url);
+        console.log("Connected correctly to server");
+        db.collection('Tasks').deleteOne({
+            '_id': new ObjectId(req.params.id)
+        }, '', function (err, result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.json(result);
+            }
+        });
+    }).catch(function (err) {
+        console.log(err.stack);
+    });
+}
+exports.deleteTask = deleteTask;
+;
+//# sourceMappingURL=apiController.js.map
